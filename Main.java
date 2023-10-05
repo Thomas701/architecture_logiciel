229ed9ace5b5a615bddb14ab4193fc2e94d4ea18
mynoteet <tho.dupois@laposte.net>
2023-10-05 13:40:31 +0200

    bases

---
+++ Main.java
@@ -0,0 +1,15 @@
+package architecture_logiciel;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) 
+    {
+        Individu[] tabInd = new Individu[20000];
+        double random = new MTRandom().nextDouble();
+        System.out.println(random);
+        
+    }
+}
---
+++ Statut.java
@@ -0,0 +1,8 @@
+package architecture_logiciel;
+
+public enum Statut {
+    SUSCEPTIBLE,
+    EXPOSED,
+    INFECTED,
+    RECOVERED;
+}
